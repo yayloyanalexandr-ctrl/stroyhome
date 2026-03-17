8eeb2d3fcf2162d382dc97b71d28d4763a6e8a53
Яйлоян Александр <yayloyanalexandr@gmail.com>
2026-03-17 13:37:07 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
   	extend: {
+  		fontFamily: {
+  			inter: ['var(--font-inter)']
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
