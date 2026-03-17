89e0ae988429c76e2940a4611a8cb2c40d9ceed7
Яйлоян Александр <yayloyanalexandr@gmail.com>
2026-03-17 15:49:15 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import ReviewsSection from '../components/landing/ReviewsSection';
 import FAQSection from '../components/landing/FAQSection';
 import ContactsSection from '../components/landing/ContactsSection';
 import Footer from '../components/landing/Footer';
+import QuizModal from '../components/landing/QuizModal';
 
 const heroImage = 'https://media.base44.com/images/public/69b958ccec4c782c15841246/92e85bad2_generated_623b2115.png';
 const aboutImage = 'https://media.base44.com/images/public/69b958ccec4c782c15841246/3a10121c5_generated_70e6ad1f.png';
