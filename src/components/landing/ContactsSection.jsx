2689bb3999a5eff7cbb0df101a5c437518202a7a
Яйлоян Александр <yayloyanalexandr@gmail.com>
2026-03-19 17:37:04 +0000

    File changes

--- src/components/landing/ContactsSection.jsx
+++ src/components/landing/ContactsSection.jsx
@@ -139,7 +139,7 @@ export default function ContactsSection() {
             {/* Карта */}
             <div className="rounded-2xl overflow-hidden border border-border flex-1 min-h-[180px]">
               <iframe
-                src="https://yandex.ru/map-widget/v1/?ll=39.701505%2C47.235714&z=16&pt=39.701505%2C47.235714,pm2rdm&org=181503572009"
+                src="https://yandex.ru/map-widget/v1/?ll=39.7138%2C47.2226&z=17&pt=39.7138%2C47.2226,pm2rdm&text=%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83%2C+%D1%83%D0%BB.+%D0%9F%D1%83%D1%88%D0%BA%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+16"
                 width="100%"
                 height="100%"
                 style={{ border: 0, minHeight: 180 }}
